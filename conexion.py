96a3946ba93136dd0ed56ebfe4ebceb72976d2bc
Tempest <dievillalba@uaa.edu.py>
2022-11-20 16:46:02 -0300

    Diego pruebas de conexiones

--- conexion.py
+++ conexion.py
@@ -4,7 +4,7 @@ con = mysql.connector.connect(
             host='localhost',
             user= 'root',
             port= 3306,
-            password = 'tuhermana',
+            password = '15022001Diego',
             database = 'uaamallacurricular'
         )
 cur = con.cursor()
