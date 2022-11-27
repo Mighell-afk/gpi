f0e25a85f9384ab56a531f3ac28a652b79c7f8a0
Mighell-afk <josem.cabreran@gmail.com>
2022-11-27 15:33:25 -0300

    noanda?

---
+++ z_modules/Materia/materia.py
@@ -0,0 +1,30 @@
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2 import QtWidgets
+import sys
+#from ..conexion import BaseDeDatos
+from Vista.Materia import Ui_Materia
+
+
+class Materia(QtWidgets.QMainWindow):
+   
+   
+    def __init__(self):
+        super(Materia, self).__init__()
+        self.materia = Ui_Materia()
+        self.materia.setupUi(self)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mi_aplicacion = Materia()
+    mi_aplicacion.show()
+    sys.exit(app.exec_())
+
